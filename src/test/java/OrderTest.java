4b05bf689d8b120e0060a9670e4d6dc1a98c6d4f
maciejbafia <123640176+maciejbafia@users.noreply.github.com>
2023-04-06 11:37:38 +0200

    Update OrderTest.java

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -54,11 +54,3 @@ public class OrderTest {
     }
 }
 
-
-//będzie logować się na tego stworzonego użytkownika,
-//wejdzie klikając w kafelek Addresses po zalogowaniu
-//kliknie w + Create new address,
-//wypełni formularz New address - dane powinny być pobierane z tabeli Examples w Gherkinie (alias, address, city, zip/postal code, country, phone),
-//sprawdzi czy dane w dodanym adresie są poprawne.
-//usunie powyższy adres klikając w "delete",
-//sprawdzi czy adres został usunięty.
